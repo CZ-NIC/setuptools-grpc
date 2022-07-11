026c286731555596a0ac5202eb0b7fe3df616c11
Jan Musílek <jan.musilek@nic.cz>
2022-07-11 14:48:15 +0200

    fixup! fixup! Add build_grpc step to setuptools build

--- setuptools_grpc/__init__.py
+++ setuptools_grpc/__init__.py
@@ -1,8 +1,3 @@
 """Setuptools grpc."""
 
 __version__ = '0.2'
-
-
-def install(dist):
-    build = dist.get_command_obj("build")
-    build.sub_commands = [("build_grpc", None)] + build.sub_commands
